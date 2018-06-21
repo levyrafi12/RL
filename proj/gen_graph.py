b61e1305ae9f6be951ed4d340db374254e1be487
levyrafi18 <raffilevy@gmail.com>
2018-06-21 17:00:17 +0300

    update

--- proj/gen_graph.py
+++ proj/gen_graph.py
@@ -22,9 +22,9 @@ def plot_data(data, color, annotate):
 		annotate_max(time_steps, data)
 
 def annotate_max(x_list, y_list):
-	x = np.array(x_list)
-	y = np.array(y_list)
-	xmax = x[np.argmax(y)]
+	tail = 500000
+	y = np.array(y_list)[-tail:]
+	xmax = len(y_list) - tail + y.argmax()
 	ymax = y.max()
 	text = "max y={:.3f}".format(ymax)
 	ax = plt.gca()
@@ -32,7 +32,7 @@ def annotate_max(x_list, y_list):
 	arrowprops = dict(arrowstyle="->",connectionstyle="angle,angleA=0,angleB=60")
 	kw = dict(xycoords='data',textcoords="axes fraction",
 		arrowprops=arrowprops, bbox=bbox_props, ha="right", va="top")
-	ax.annotate(text, xy=(xmax, ymax), xytext=(0.74,0.96), **kw)
+	ax.annotate(text, xy=(xmax, ymax), xytext=(0.54,0.98), **kw)
 
 def q1_gen_graph(stat_fn, title):
 	init_plot()
@@ -46,11 +46,11 @@ def q1_gen_graph(stat_fn, title):
 	plt.legend(handles=[red_patch, blue_patch], loc=4)
 	plt.title(title)
 
-	start, end = plt.gca().get_ylim()
-	plt.yticks(np.arange(-22.0, end + 5, 2))
+	# start, end = plt.gca().get_ylim()
+	plt.yticks(np.arange(-22, 24, 2.5))
 
-	# plt.savefig('learn_curve_def_params.png')
-	plt.show()
+	plt.savefig('best_mean_rew_def_params.png')
+	# plt.show()
 
 def q2_gen_graph(stat_files, colors, key, labels, title, out_pref, annotate):
 	init_plot()
@@ -63,24 +63,25 @@ def q2_gen_graph(stat_files, colors, key, labels, title, out_pref, annotate):
 	
 	plt.legend(handles=color_patches, loc=4)
 	plt.title(title)
-	# plt.savefig(out_pref + '_modified_params.png')
-	plt.show()
+	plt.yticks(np.arange(-22, 26.5, 2.5))
+	plt.savefig(out_pref + '_mod_params.png')
+	# plt.show()
 
 if __name__ == '__main__':
-	q1_gen_graph('statistics_def.pkl', 'mean and best mean rewards using default params')
+	q1_gen_graph('statistics_01.pkl', 'mean and best mean rewards using default params')
 
-	"""
-	files = ['statistics_def.pkl', 'statistics_005.pkl', 'statistics_00.pkl']
-	colors = ['red', 'blue', 'green']
-	labels = ['final_p = 0.1', 'final_p = 0.05', 'final_p=0.0']
-	annotate = [False, False, True]
+	files = ['statistics_01.pkl', 'statistics_005.pkl', 'statistics_0025.pkl', 'statistics_00.pkl']
+	colors = ['red', 'blue', 'green', 'yellow']
+	labels = ['final_p = 0.1', 'final_p = 0.05', 'final_p = 0.025', 'final_p = 0.0']
+	annotate = [False, False, False, True]
 
 	q2_gen_graph(files, colors, 'mean_episode_rewards', labels, 
-		'mean reward using different final_p', 'mean_rew', annotate) 
+		'mean reward using different final exploration probabilty (final_p)', 
+		'mean_rew', annotate) 
 
 	q2_gen_graph(files, colors, 'best_mean_episode_rewards', labels, 
-		'best mean reward using different final_p', 'best_mean_rew', annotate) 
-	"""
+		'best mean reward using different final exploration probability (final_p)', 
+		'best_mean_rew', annotate) 
 
 
 
